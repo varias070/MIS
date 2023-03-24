856b625fd544e0973f2ac64b377e22bd06a5fddb
dev <varias070@gmail.com>
2023-03-24 15:30:54 +0700

    изменил readme добавил хост в докерфайл

--- README.md
+++ README.md
@@ -10,23 +10,33 @@
 2. Выполнить 
 
    ```bash
-    1 docker-compose up db
-    2 docker-compose up app
-    3 docker-compose exec app node filler.js
+    docker-compose pool
+    docker-compose build 
+    docker-compose up db
+    docker-compose up app
+    docker-compose exec app node filler.js
    ```
 3.  docker-compose exec app node filler.js выполняется только при первом запуске, дальнейший запуск происходит командой
    ```bash
    docker-compose up
    ```
 
-4. Сценарий использование
-   
-   ```bash
+4. Сценарий использования
+
+   Для удобного тестирование все запросы имеют метод POST(конечно в производственной среде это не допустимо)
+
    1 для получение расписание нужно отправить запрос http://127.0.0.1:3000/get_schedule с запросом необходимо передать джсон {"doctor_id": 2, "patient_id": null, "date": '3-9-2023 ', "time_from": '20-30', "is_free": true,}
+
    2 создать специальность http://127.0.0.1:3000/create_spec и передать джсон {"title": "Surgeon"}
+
    3 создать врача http://127.0.0.1:3000/create_doctor и передать джсон {"name": "Ivan", "spec_id": 1, "price": 1000}
+
    4 создать пациента http://127.0.0.1:3000/create_patient и передать джсон  {"phone": "+7 913 743 24 35", "name": "Jason", "email": "jason@examaple.com", "gender": "male",}
+
    5 создать расписание для врача http://127.0.0.1:3000/create_schedule и передать джсон {"doctor_id": 1,"date": "2018-04-04T09:00",} создается 24 приема  с 9:00 до 21:00 по 30 минут каждый
+
    6 получить расписание http://127.0.0.1:3000/get_schedule и передать параметры поиска {"doctor_id": 2, "patient_id": null, "date": '3-9-2023 ', "time_from": '20-30', "is_free": true,} 
+
    7 для записи на прием необходимо отправить запрос http://127.0.0.1:3000/make_appointment и джсон {"id": 7, "patient_id": 1, "type": 0} поле type может принимать значение 0-первичный прием и 1-вторичный прием
-   ```
+
+   более подробные примеры содержатся в файле req.js
--- req.js
+++ req.js
@@ -13,47 +13,50 @@ async function fetchAsync (url, payload) {
     console.log(data);
 }
 
-//let patient = {
-//    "phone": "+7 913 743 24 35",
-//    "name": "Jason",
-//    "email": "jason@examaple.com",
-//    "gender": "male",
-//}
-//fetchAsync("http://127.0.0.1:3000/create_patient", patient)
+let patient = {
+    "phone": "+7 913 743 24 35",
+    "name": "Jason",
+    "email": "jason@examaple.com",
+    "gender": "male",
+}
+fetchAsync("http://127.0.0.1:3000/create_patient", patient)
 
-//let doctor = {
-//    "name": "Ivan",
-//    "spec_id": 1,
-//    "price": 1000
-//}
-//fetchAsync("http://127.0.0.1:3000/create_doctor", doctor)
+let doctor = {
+    "name": "Ivan",
+    "spec_id": 1,
+    "price": 1000
+}
+fetchAsync("http://127.0.0.1:3000/create_doctor", doctor)
 
-//let appointment = {
-//    "date": "2018-04-04T14:00",
-//    "doctor": 1,
-//    "patient": 1,
-//    "type": 0,
-//    "is_free": false,
-//}
-//fetchAsync("http://127.0.0.1:3000/create_appointment", appointment)
+let appointment = {
+    "date": "2018-04-04T14:00",
+    "doctor": 1,
+    "patient": 1,
+    "type": 0,
+    "is_free": false,
+}
+fetchAsync("http://127.0.0.1:3000/create_appointment", appointment)
 
-//let create_schedule = {
-//    "doctor_id": 1,
-//    "date": "2018-04-04T09:00",
-//}
-//fetchAsync("http://127.0.0.1:3000/create_schedule", create_schedule)
-//let schedule = {
-//    "doctor_id": 2,
-//    "patient_id": null,
-//    "date": '3-9-2023 ',
-//    "time_from": '20-30',
-//    "is_free": true,
-//}
-//fetchAsync("http://127.0.0.1:3000/get_schedule", schedule)
-//
-//let appointment_update = {
-//    "id": 7,
-//    "patient_id": 1,
-//    "type": 0
-//}
-//fetchAsync("http://127.0.0.1:3000/make_appointment", appointment_update)
+let create_schedule = {
+    "doctor_id": 1,
+    "date": "2018-04-04T09:00",
+}
+fetchAsync("http://127.0.0.1:3000/create_schedule", create_schedule)
+let schedule = {
+    "doctor_id": 2,
+    "patient_id": null,
+    "date": '3-9-2023 ',
+    "time_from": '20-30',
+    "is_free": true,
+}
+fetchAsync("http://127.0.0.1:3000/get_schedule", schedule)
+
+let appointment_update = {
+    "id": 7,
+    "patient_id": 1,
+    "type": 0
+}
+fetchAsync("http://127.0.0.1:3000/make_appointment", appointment_update)
+
+spec = {"title": "Surgeon"}
+fetchAsync("http://127.0.0.1:3000/create_spec", spec)
